d9cfc0abec8573bc2ddab5c969555aef10069551
shimizu <xxshimizuxx@gmail.com>
2024-10-03 17:13:56 +0900

    terrainシェーダーを調整

--- examples/world_creator_export/src/Scene.jsx
+++ examples/world_creator_export/src/Scene.jsx
@@ -6,112 +6,92 @@ import { useFrame, extend, useLoader } from "@react-three/fiber";
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
 import chroma from 'chroma-js';
 
-
+import { vertexShader, fragmentShader } from "./shader.js"
 
 function Terrain() {
 
-    const { scene } = useGLTF('./terrain/Terrain_Mesh_0_0.glb');
-    const colorMap = useLoader(THREE.TextureLoader, './terrain/Terrain_Colormap_0_0.png');
+    // PlaneGeometryを生成
+    const planeGeometry = useMemo(() => new THREE.PlaneGeometry(10, 10, 1024, 1024), []);
+
+
+    const heightMap = useLoader(THREE.TextureLoader, './terrain/Terrain_Height Map_1024x1024_0_0.png');
+    const colorMap = useLoader(THREE.TextureLoader, './terrain/Terrain_Colormap_0_0.jpg');
     const normalMap = useLoader(THREE.TextureLoader, './terrain/Terrain_Normal Map_0_0.png');
-    const roughnessMap = useLoader(THREE.TextureLoader, './terrain/Terrain_Roughness Map_0_0.png');
-    const metalnessMap = useLoader(THREE.TextureLoader, './terrain/Terrain_Metalness Map_0_0.png');
 
+    const splatMap0 = useLoader(THREE.TextureLoader, './terrain/Terrain_Splat Map_0_0_0.png');
+    const splatMap1 = useLoader(THREE.TextureLoader, './terrain/Terrain_Splat Map_1_0_0.png');
+    const splatMap2 = useLoader(THREE.TextureLoader, './terrain/Terrain_Splat Map_2_0_0.png');
+    const splatMap3 = useLoader(THREE.TextureLoader, './terrain/Terrain_Splat Map_3_0_0.png');
+    const splatMap4 = useLoader(THREE.TextureLoader, './terrain/Terrain_Splat Map_4_0_0.png');
 
-    // テクスチャの読み込み
-    const splatMap = useLoader(THREE.TextureLoader, './terrain/Terrain_Splat Map_2_0_0.png');
     const grassTexture = useLoader(THREE.TextureLoader, './texture/grass.png');
-
-    // テクスチャの繰り返し設定
     grassTexture.wrapS = THREE.RepeatWrapping;
     grassTexture.wrapT = THREE.RepeatWrapping;
-    grassTexture.repeat.set(4, 4); // 4x4繰り返し適用
+
+    const rockTexture = useLoader(THREE.TextureLoader, './texture/texture512.jpg');
+    rockTexture.wrapS = THREE.RepeatWrapping;
+    rockTexture.wrapT = THREE.RepeatWrapping;
 
 
-    const rockTexture = useLoader(THREE.TextureLoader, './texture/Stone 4.jpg');
-
-    // 使用するテクスチャをメモ化
+    // ShaderMaterialをメモ化
     const material = useMemo(() => {
         return new THREE.ShaderMaterial({
             uniforms: {
+                time: { value: 0 },
+                heightMap: { value: heightMap },
                 colorMap: { value: colorMap },
-                splatMap: { value: splatMap },
+                splatMap0: { value: splatMap0 },
+                splatMap1: { value: splatMap1 },
+                splatMap2: { value: splatMap2 },
+                splatMap3: { value: splatMap3 },
+                splatMap4: { value: splatMap4 },
                 rockTexture: { value: rockTexture },
                 grassTexture: { value: grassTexture },
                 normalMap: { value: normalMap },
-                roughnessMap: { value: roughnessMap },
-                metalnessMap: { value: metalnessMap },
+
+                normalScale: { value: 2.0 }, // 法線マップの強度を調整
+
+                textureRepeat: { value: new THREE.Vector2(24, 24) }, // リピート係数
+
+                displacementScale: { value: 5.0 }, // 高さのスケール
+
+                // ライト関連のuniformを追加
+                lightPosition: { value: new THREE.Vector3(0, 0, 0) },  // ライトの位置
+                lightColor: { value: new THREE.Color(1, 1, 1) },          // ライトの色
+                ambientColor: { value: new THREE.Color(0.1, 0.1, 0.1) },  // 環境光の色
             },
-            vertexShader: `
-            varying vec2 vUv;
-            varying vec3 vNormal;
-            void main() {
-            vUv = uv;
-            vNormal = normalize(normalMatrix * normal);
-            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-            }
-          `,
-            fragmentShader: `
-            varying vec2 vUv;
-            varying vec3 vNormal;
-            uniform sampler2D colorMap;
-            uniform sampler2D splatMap;
-            uniform sampler2D normalMap;
-            uniform sampler2D roughnessMap;
-            uniform sampler2D metalnessMap;
-
-            uniform sampler2D rockTexture;
-            uniform sampler2D grassTexture;
-
-            void main() {
-            // ベースカラーの取得
-            vec4 baseColor = texture2D(colorMap, vUv);
-
-            // スプラットマップの取得
-            vec4 splat = texture2D(splatMap, vUv);
-             float splatValue = texture2D(splatMap, vUv).r;
-
-            // 各テクスチャの取得
-            vec4 rockColor = texture2D(rockTexture, vUv);
-            vec4 grassColor = texture2D(grassTexture, vUv);
-
-            // Splat Mapに基づいたブレンド (0: baseColor1: rockColor)
-       
-            vec4 blendedColor = mix(grassColor, baseColor, splatValue);
-
-            // 正規化（ブレンド係数の合計が1になるように調整） 
-            float total = splat.r + splat.g + splat.b + splat.a;
-            blendedColor /= total;
-                
-
-            // 最終カラーの設定
-            gl_FragColor = vec4(blendedColor.rgb, 1.0);
-            }
-
-        `,
+            vertexShader: vertexShader,
+            fragmentShader: fragmentShader,
             transparent: false,
         });
-    }, [colorMap, splatMap, normalMap, roughnessMap, metalnessMap]);
+    }, [colorMap, splatMap0, splatMap1, splatMap2, splatMap3, splatMap4, normalMap]);
 
-
-    scene.traverse((child) => {
-        if (child.isMesh) {
-            child.material = material; // カスタムマテリアルを適用
-        }
+    // フレームごとにtimeを更新する
+    useFrame((state) => {
+        material.uniforms.time.value = state.clock.getElapsedTime();
+        material.uniforms.lightPosition.value = new THREE.Vector3(10, 10, 0); // ライトの位置
+        material.uniforms.lightColor.value = new THREE.Color(1, 1, 1);          // ライトの色
+        material.uniforms.ambientColor.value = new THREE.Color(0.5, 0.5, 0.5);  // 環境光
     });
 
+    const mesh = new THREE.Mesh(planeGeometry, material);
 
-    /*
-    scene.traverse((child) => {
-        if (child.isMesh) {
-            child.material.map = colorMap;
-            child.material.normalMap = normalMap;
-            child.material.roughnessMap = roughnessMap;
-            child.material.metalnessMap = metalnessMap;
-        }
-    });
-    */
+    return <primitive object={mesh} />;
+}
 
-    return <primitive object={scene} />;
+
+function Terrain2(){
+
+    // PlaneGeometryを生成
+    const planeGeometry = useMemo(() => new THREE.PlaneGeometry(10, 10, 1024, 1024), []);
+
+    const material = new THREE.MeshBasicMaterial()
+    material.color = new THREE.Color(0x1188ff)
+
+    const mesh = new THREE.Mesh(planeGeometry, material);
+
+
+    return <primitive object={mesh}  />;
 }
 
 
@@ -124,20 +104,12 @@ function Scene(){
 
             <OrbitControls />
 
-            <RandomizedLight />
-
-
-            <mesh>
-                <boxGeometry args={[1,1,1]} />
-                <meshBasicMaterial color={0xff0000} />
-            </mesh>
-
-            <group scale={0.01}>
-                <Terrain />
-            </group>
+            <Terrain />
                         
         </>
     )
 }
 
 export default Scene
+
+
